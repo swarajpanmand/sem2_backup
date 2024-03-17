5afa4320d53fefb854289a56b8155243f7f5193a
Swaraj Panmand <69612843+swarajpanmand@users.noreply.github.com>
2024-03-17 11:09:40 +0000

    updating files

--- exp3/que1.java
+++ exp3/que1.java
@@ -49,40 +49,4 @@ class Date {
         d1.display();
     }
 }
-*/
-/* /* 
-package exp3;
-import java.util.Scanner;
-public class que1 {
-    int id;
-    int age;
-    int salary;
-    String name;
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter ur name: ");
-    String name = sc.next();
-    System.out.println("Enter ur age: ");
-    int age = sc.nextInt();
-    System.out.println("Enter ur id  ");
-    int id = sc.nextInt();
-    System.out.println("Enter ur salary:");
-    int salary = sc.nextInt();
-    que1 q1= new que1(name, age, id, salary);
-    q1.getdisplay();
-}
-que1(String name, int age, int id, int salary){
-this.id = id;
-this.name = name;
-this.age = age;
-this.salary = salary;
-}
-void getdisplay(){
-System.out.println("Name: "+name);
-System.out.println("Age: "+age);
-System.out.println("Id: "+id);
-System.out.println("Salary: "+salary);
-}
-}
-
 
